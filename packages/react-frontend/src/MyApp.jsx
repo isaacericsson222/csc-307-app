fe2c83c48e23cced3c244bba1fbea78fc04907bf
isaacericsson222 <isaac.h.ericsson@gmail.com>
2025-09-28 14:04:53 -0700

    finished input fields

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,43 +1,15 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
-const characters = [
-  {
-    name: "Charlie",
-    job: "Janitor"
-  },
-  {
-    name: "Mac",
-    job: "Bouncer"
-  },
-  {
-    name: "Dee",
-    job: "Aspring actress"
-  },
-  {
-    name: "Dennis",
-    job: "Bartender"
-  }
-];
+
 
 function MyApp() {
   const [characters, setCharacters] = useState([
-    {
-    name: "Charlie",
-    job: "Janitor"
-  },
-  {
-    name: "Mac",
-    job: "Bouncer"
-  },
-  {
-    name: "Dee",
-    job: "Aspring actress"
-  },
-  {
-    name: "Dennis",
-    job: "Bartender"
-  }
+    { name: "Charlie", job: "Janitor" },
+    { name: "Mac", job: "Bouncer" },
+    { name: "Dee", job: "Aspiring actress" }, // fixed typo
+    { name: "Dennis", job: "Bartender" },
   ]);
 
   function removeOneCharacter(index) {
@@ -53,6 +25,7 @@ function MyApp() {
       characterData={characters}
       removeCharacter={removeOneCharacter}
     />
+    <Form />
   </div>
 );
 }
