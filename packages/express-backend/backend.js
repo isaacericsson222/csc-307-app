bbd3e9f83c7a3c53a189da83705190bcb71c0b7b
isaacericsson222 <isaac.h.ericsson@gmail.com>
2025-10-06 13:10:10 -0700

    finished get user from id 2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -45,6 +45,12 @@ const findUserByName = (name) => {
 };
 
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+
+
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -61,6 +67,15 @@ app.get("/users", (req, res) => {
   }
 });
 
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
 
 app.listen(port, () => {
   console.log(
