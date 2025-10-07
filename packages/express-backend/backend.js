53b32836ea6412254f6a5477147e6c24e8927ba9
isaacericsson222 <isaac.h.ericsson@gmail.com>
2025-10-07 15:47:12 -0700

    backend setup 3

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,7 +6,6 @@ const port = 8000;
 
 app.use(express.json());
 
-
 const users = {
   users_list: [
     {
@@ -37,28 +36,57 @@ const users = {
   ]
 };
 
-
 const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name
   );
 };
 
-
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job
+  );
+};
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
 
+const deleteUserById = (id) => {
+  const index = users["users_list"].findIndex(
+    (user) => user["id"] === id
+  );
+  if (index !== -1) {
+    users["users_list"].splice(index, 1);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job;
 
-    const name = req.query.name;
-  if (name != undefined) {
+  if (name != undefined && job != undefined) {
+    let result = findUserByNameAndJob(name, job);
+    result = { users_list: result };
+    res.send(result);
+  } else if (name != undefined) {
     let result = findUserByName(name);
     result = { users_list: result };
     res.send(result);
@@ -74,6 +102,17 @@ app.get("/users/:id", (req, res) => {
     res.status(404).send("Resource not found.");
   } else {
     res.send(result);
+  }
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  const success = deleteUserById(id);
+
+  if (success) {
+    res.status(204).send(); // No content
+  } else {
+    res.status(404).send("Resource not found.");
   }
 });
 
